e2b76b22da1174c9f4040d43b60e745ac1f059c1
jesuschock93 <jesuschock93@gmail.com>
2023-05-10 11:50:14 -0400

    services-end

--- src/movies/movies.services.js
+++ src/movies/movies.services.js
@@ -1,2 +1,31 @@
-const moviesControllers = require('./movies.controllers')
+const moviesControllers = require('./movies.controllers');
 
+const getAllMovies = (req, res) => {
+    moviesControllers.getAllMovies()
+        .then( r => res.status(200).json(r))
+        .catch(err => res.status(400).json({ message: err.message }));
+};
+
+const postMovie = (req,res) => {
+    const  data = req.body;
+    if (data.name && data.genre && data.duration && data.releaseDate ) {
+        moviesControllers.createMovie(data)
+            .then(r => res.status(201).json())
+            .catch(err => res.status(400).json({message: err.message}))
+    } else {
+        res.status(400).json({message: 'Missing Data'})
+    };
+};
+
+const getMovieById = (req, res) => {
+    const id = req.params.id
+    moviesControllers.getMovieById(id)
+        .then(r => res.status(200).json({r}))
+        .catch(err => res.status(404).json({message: err.message}))
+};
+
+module.exports = {
+    getAllMovies,
+    getMovieById,
+    postMovie
+}
